b4a19dda853ff374e59e786bbe53cbdfe274984e
Alexander Wolff <wolffaxn@gmail.com>
2020-10-18 10:53:51 +0200

    support musl libc to allow efficient static linking (#11)

--- example/main.go
+++ example/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 )
 
+func echo(args []string) error {
+	if len(args) < 1 {
+		return errors.New("Too few arguments")
+	}
+	_, err := fmt.Println(strings.Join(args[:], " "))
+	return err
+}
+
 func main() {
-	fmt.Println("hello, world")
+	echo([]string{"hello,", "world"})
 }
